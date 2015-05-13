ee58b430f9e2fb4826670d5531e9e76c421c8543
JMtorii <j.torii4@gmail.com>
2015-05-12 22:47:51 -0400

    Daily update

---
+++ about/story.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include '../helpers/head_content.php';?>
+        <script src="../js/init_story.js"></script>
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="http://juntorii.com/index.php">home</a></li>
+                        <li class="active"><a href="http://juntorii.com/about/story.php">story</a></li>
+                        <li><a href="http://juntorii.com/about/work.php">work</a></li>
+                        <li><a href="http://juntorii.com/about/resume.php">resume</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+<!--         <div class="jumbotron">
+            <div class="container">
+                <h1>My Story</h1>
+            </div>
+        </div> -->
+
+        <section id="intro" data-speed="6" data-type="background">
+            <div class="container">
+                <h1>Jun Mark Torii</h1>
+            </div>
+        </section>
+
+        <section id="home" data-speed="4" data-type="background">
+            <div class="container">
+                <p>Originally from a visual arts and music background, Mark Torii has transitioned from the arts to engineering and computer science.</p>
+                <p>Mark is a Mississauga-raised Canadian who is originally Japanese and Korean. He is a video game collector and gamer, enjoys TV shows and movies, and loves watching NBA games. Mark went to Floradale Public School for elementary school then enrolled in various schools for middle and high school. After many successful academic years, Mark continued his education in the University of Waterloo in the Software Engineering Co-op program. He has had the opportunity to experience different co-op jobs and work on many personal projects.</p>
+            </div>
+        </section>
+
+        <hr class="divider"></hr>
+
+        <section id="about" data-speed="2" data-type="background">
+            <div class="container">
+                Final section.
+            </div>
+        </section>
+
+
+<!--         <div class="container story">
+            <div class="row">
+                <h1>hi</h1>
+            </div>
+
+            <hr class="divider"></hr>
+
+            <div class="row">
+                <h1>hi</h1>
+            </div>
+
+            <hr class="divider"></hr>
+
+            <div class="row">
+                <h1>hi</h1>
+            </div>
+        </div> -->
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="../js/jquery-2.1.3.min.js"></script>
+        <script src="../bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <!--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'helpers/head_content.php';?>
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="http://juntorii.com/index.php">home</a></li>
+                        <li><a href="http://juntorii.com/about/story.php">story</a></li>
+                        <li><a href="http://juntorii.com/about/work.php">work</a></li>
+                        <li><a href="http://juntorii.com/about/resume.php">resume</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="main-container">
+                <h2 id="first">hi. i'm <i>mark torii</i>.</h2>
+                <h2 id="second">and i am a</h2>
+                <h1>gamer,<br>designer,<br>engineer,<br>software developer</h1>
+            </div>
+        </div>
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="jquery-2.1.3.min.js"></script>
+        <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <!--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
+    </body>
+</html>
